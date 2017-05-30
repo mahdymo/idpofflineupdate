2e7ffce955f8a4b83ac587b40183994db78c0c66
Mahdy <mohamed.mahdy4@gmail.com>
2017-05-30 09:20:39 +0300

    Update the script
    Optimizing the script through the use of multiple xpaths

--- idpofflineupdate.py
+++ idpofflineupdate.py
@@ -1,45 +1,30 @@
 ###############################################################################################
 ########################### The purpose of this script is to check the latest IDP update#######
+########################### Author mmahdy #####################################################
 ###############################################################################################
 
 from lxml import etree
 import urllib
 import os
 
-
 def idpupdate(device, OS, location, build):
     manifesturl = "https://signatures.juniper.net/cgi-bin/index.cgi?type=manifest&device=%s&feature=ai&detector=0.0.0&" \
                   "to=latest&os=%s&build=%s" % (device, OS, build)
-
     urllib.urlretrieve(manifesturl, '%s/manifest.xml' % location)
-
-    ##### Remove tags##########
-    tree = etree.parse('%s/manifest.xml' % location)
-    notags = etree.tostring(tree, encoding='utf8', method='text')
-    DB_List = notags.split()
-    DB_List = map(lambda s: s.strip(), DB_List)
-
-    ####Signature update get the url#######
-
-    signatures = 'SignatureUpdate.xml.gz'
-    index = DB_List.index(signatures)
-    urlindex = index + 5
-    SIGDBURL = DB_List[urlindex]
-    urllib.urlretrieve(SIGDBURL, '%s/SignatureUpdate.xml' % location)
-
-    ######Check the IDP Version############
-    signaturetree = etree.parse('%s/SignatureUpdate.xml' % location)
-    element = signaturetree.xpath('//SignatureUpdate/UpdateNumber')
-    for i in element:
-        version = i.text
-
+    signaturetree = etree.parse('%s/manifest.xml' % location)
+    element = signaturetree.xpath('/manifest/version')
+    for x in element:
+        version = x.text
     Cont_Update = raw_input("The available version is %s do you wish to proceed (y or n): " % version)
     Cont_Update = Cont_Update.upper()
+
+    SIGDBURL = "https://signatures.juniper.net/xmlupdate/225/SignatureUpdates/%s/SignatureUpdate.xml.gz" %version
+    urllib.urlretrieve(SIGDBURL, '%s/SignatureUpdate.xml' % location)
 
     Files = ['ApplicationGroups', 'ApplicationGroups2', 'ApplicationSchema', 'Applications', 'Applications2',
              'Detector',
              'Groups', 'Heuristics', 'Libqmprotocols', 'Platforms']
-
+    signaturetree = etree.parse('%s/SignatureUpdate.xml' % location)
     if Cont_Update == 'Y':
         for x in Files:
             element = signaturetree.xpath('//SignatureUpdate/%s' % x)
