53ea93b98382d714b58d4d6fc0214c8bf22a03c0
Mahdy <mohamed.mahdy4@gmail.com>
2017-06-13 01:35:50 +0300

    Updating the script to perform auto detection
    Updating the script to perform auto detection and comparison, you will need to add the SRX list of IPs manually at the code and it will act upon

--- idpofflineupdate.py
+++ idpofflineupdate.py
@@ -6,8 +6,14 @@
 from lxml import etree
 import urllib
 import os
+import paramiko
+import getpass
 
-def idpupdate(device, OS, location, build):
+
+########## The used functions ######################
+
+def idpupdate(device, OS, location, build,currentversion,l):
+
     manifesturl = "https://signatures.juniper.net/cgi-bin/index.cgi?type=manifest&device=%s&feature=ai&detector=0.0.0&" \
                   "to=latest&os=%s&build=%s" % (device, OS, build)
     urllib.urlretrieve(manifesturl, '%s/manifest.xml' % location)
@@ -15,9 +21,10 @@ def idpupdate(device, OS, location, build):
     element = signaturetree.xpath('/manifest/version')
     for x in element:
         version = x.text
-    Cont_Update = raw_input("The available version is %s do you wish to proceed (y or n): " % version)
-    Cont_Update = Cont_Update.upper()
-    if Cont_Update == 'Y':
+
+    if version > currentversion:
+    	print "Device : "+l
+        print "The current version is "+ currentversion + " there's a newer version "+ version + " and it is being downloaded ... "
         SIGDBURL = "https://signatures.juniper.net/xmlupdate/225/SignatureUpdates/%s/SignatureUpdate.xml.gz" %version
         urllib.urlretrieve(SIGDBURL, '%s/SignatureUpdate.xml' % location)
 
@@ -29,22 +36,70 @@ def idpupdate(device, OS, location, build):
             element = signaturetree.xpath('//SignatureUpdate/%s' % x)
             for j in element:
                 url = j.text
-                filename = url.split('/')[-1]
+                filename = l+"-"+url.split('/')[-1]
                 urllib.urlretrieve(url, '%s/%s' % (location, filename))
 
     print "Download completed, unzip files ..."
     loccommand = location+"/"+"*.gz"
     command = "gzip -d %s" % loccommand
-
     os.system(command)
+    return ()
 
+###############Automatically get the device information #######################################
+##########This function will get device model, OS, build, idp security package version ########
+###############################################################################################
+
+
+def getdeviceinfo(l,location):
+    SRX = paramiko.SSHClient()
+    SRX.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    SRX.connect(l, username=unsername, password=password)
+    din, dout, derr = SRX.exec_command("show version detail | display xml| no-more")
+    din, dout2, derr = SRX.exec_command("show security idp security-package-version | display xml | no-more")
+    file2write = open("%s/tmpoutput.xml" %location, 'w')
+    file2write.write(dout.read())
+    file2write.close()
+    signaturetree = etree.parse("%s/tmpoutput.xml" %location)
+    prodelement = signaturetree.xpath('//software-information/product-name')
+    relelement = signaturetree.xpath('//version-information/release')
+    file2write = open("%s/tmpoutput.xml" %location, 'w')
+    file2write.write(dout2.read())
+    file2write.close()
+    signaturetree = etree.parse("%s/tmpoutput.xml" %location)
+    verelement = signaturetree.xpath('//security-package-version')
+    version.append(verelement[0].text[:verelement[0].text.index('(')])
+    device.append(prodelement[0].text)
+    OS.append(relelement[0].text[:relelement[0].text.index('X')])
+    Build.append(relelement[0].text[relelement[0].text.index('D') + 1:].split('.')[0])
+    SRX.close()
     return ()
 
 
-device = raw_input('Please provide the device model(srx1400, srx5400, srx5800): ')
-OS = raw_input('Please provide the current version of the FW(15.1 , 12.3 , etc..): ')
-build = raw_input('Please provide the current build of the FW(44, 70 , 80): ')
+
+
+################# The code #########################
+global device
+global OS
+global Build
+global version
+
+device = []
+OS = []
+Build = []
+version = []
+unsername = raw_input("Please provide your username: ")
+password = getpass.getpass("Please enter the password: ")
 location = raw_input('Where to save the files: ')
-idpupdate(device, OS, location, build)
+
+List_PoP = "You need to define the list of Devices you have in the Network here" ### <<<<<<<<<<< 
+for l in List_PoP:
+    getdeviceinfo(l,location)
+
+count = 0
+for l in List_PoP:
+
+    idpupdate(device[count], OS[count], location, Build[count],version[count],l)
+    count +=1
+
 
 print "Completed, please copy the files to the firewall idp location"
