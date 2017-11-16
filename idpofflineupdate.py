fc78bdfd3ba3f63cddd883337d5e2c2913c627e4
Mahdy <mohamed.mahdy4@gmail.com>
2017-11-16 20:19:36 +0300

    Update the download link to include IDP
    Update the download link to include IDP 
    instead of AI only , we are adding the link for IDP 
    Moving the files to the firewalls directory /var/tmp/IDP

--- idpofflineupdate.py
+++ idpofflineupdate.py
@@ -35,44 +35,38 @@ def sysarguments():
                         loc = sys.argv[locaindex]
                         location.append(loc)
                 if arg == "-h":
-						print "-u : Username \n-o : The output folder \n-i : The node/nodes name or IP \n"
-						sys.exit(1)
+			print "-u : Username \n-o : The output folder \n-i : The node/nodes name or IP \n"
+			sys.exit(1)
 
         return()
 
 ########## The used functions ######################
 
-
-
 def idpupdate(device, OS, location, build,currentversion,l):
-	mk = "mkdir %s"%l
+	mk = "mkdir -p /var/tmp/IDPFiles/%s"%l
 	os.system(mk)
 	manifesturl = "https://signatures.juniper.net/cgi-bin/index.cgi?device=%s&feature=idp&detector=12.6.140170603&from=&to=latest&os=%s&build=%s&type=update" % (device, OS, build)
-	urllib.urlretrieve(manifesturl, '%s/%s/SignatureUpdate.xml.gz' % (location,l))
-	quickcommand="gzip -df %s/%s/SignatureUpdate.xml.gz"% (location,l)
+	urllib.urlretrieve(manifesturl, '/var/tmp/IDPFiles/%s/SignatureUpdate.xml.gz' %l)
+	quickcommand="gzip -f -d /var/tmp/IDPFiles/%s/SignatureUpdate.xml.gz"%l
 	os.system(quickcommand)
-	signaturetree = etree.parse('%s/%s/SignatureUpdate.xml' % (location,l))
+	signaturetree = etree.parse('/var/tmp/IDPFiles/%s/SignatureUpdate.xml' %l)
 	element = signaturetree.xpath('/SignatureUpdate/UpdateNumber')
 	for x in element:
 		version = x.text
-	
-	if version > currentversion:
-		print "Device : "+l
-		print "The current version is "+ currentversion + " there's a newer version "+ version + " and it is being downloaded ... "
-        SIGDBURL = "https://signatures.juniper.net/xmlupdate/225/SignatureUpdates/%s/SignatureUpdate.xml.gz" %version
-        urllib.urlretrieve(SIGDBURL, '%s/%s/SignatureUpdate.xml' % (location,l))
-
+	version_List.append(version)
+	#SIGDBURL = "https://signatures.juniper.net/xmlupdate/225/SignatureUpdates/%s/SignatureUpdate.xml.gz" %version
+	#urllib.urlretrieve(SIGDBURL, '/var/tmp/IDPFiles/%s/SignatureUpdate.xml.gz' %l)
+	#os.system(quickcommand)
 	Files = ['ApplicationGroups', 'ApplicationGroups2', 'ApplicationSchema', 'Applications', 'Applications2','Detector','Groups', 'Heuristics', 'Libqmprotocols', 'Platforms','Templates']
-        signaturetree = etree.parse('%s/%s/SignatureUpdate.xml' % (location,l))
-        for x in Files:
-            element = signaturetree.xpath('//SignatureUpdate/%s' % x)
-            for j in element:
-                url = j.text
-                filename = url.split('/')[-1]
-                urllib.urlretrieve(url, '%s/%s/%s' % (location,l, filename))
-
-	loccommand = location+"/"+l+"/"+"*.gz"
-	command = "gzip -d %s" % loccommand
+	signaturetree = etree.parse('/var/tmp/IDPFiles/%s/SignatureUpdate.xml' %l)
+	for x in Files:
+		element2 = signaturetree.xpath('/SignatureUpdate/%s' % x)
+        	for j in element2:
+        		url = j.text
+        		filename = url.split('/')[-1]
+        		urllib.urlretrieve(url, '/var/tmp/IDPFiles/%s/%s' % (l, filename))
+	loccommand = "/var/tmp/IDPFiles"+"/"+l+"/"+"*.gz"
+	command = "gzip -f -d %s" % loccommand
 	os.system(command)
 	return ()
 
@@ -82,7 +76,7 @@ def idpupdate(device, OS, location, build,currentversion,l):
 
 
 def getdeviceinfo(l,location,password):
-	conn =  manager.connect(host=l, port=22, username=username[0], password=password,hostkey_verify=False, device_params={'name':'junos'})
+	conn =  manager.connect(host=l, port=22, username=username, password=password,hostkey_verify=False, device_params={'name':'junos'})
 	c = new_ele('get-software-information')
 	c2 = new_ele('get-idp-security-package-information')
 	result = conn.rpc(c)
@@ -90,12 +84,37 @@ def getdeviceinfo(l,location,password):
 	prodelement = result.xpath('//software-information/product-name')[0].text
 	relelement = result.xpath('//software-information/package-information/comment')[0].text
 	verelement = result2.xpath('//security-package-version')[0].text
-	version.append(verelement[:verelement.index('(')])
+	Currentversion_List.append(verelement[:verelement.index('(')])
 	device.append(prodelement)
 	OS.append(relelement[relelement.index('[')+1:relelement.index('X')])
 	Build.append(relelement[relelement.index('D') + 1:relelement.index(']')])
 	return ()
 
+def movefiles_to_firewalls(username,pw,List_IPs):
+	port = 22
+	CPFILES = ["SignatureUpdate.xml","application_groups.xml","application_groups2.xml","applications.xsd","applications.xml","applications2.xml","libidp-detector.so.tgz.v","groups.xml","heuristics.bin","libqmprotocols.tgz","platforms.xml","templates.xml"]
+	for l in List_IPs:
+		transport = paramiko.Transport((l, port))
+		# Auth
+		transport.connect(username = username, password = pw)
+		# Go!
+		SFTP = paramiko.SFTPClient.from_transport(transport)
+		
+		# Upload
+		try:
+			SFTP.rmdir('/var/tmp/IDP')
+		except:
+			try:
+				SFTP.mkdir('/var/tmp/IDP')
+			except:
+				SFTP.chdir('/var/tmp/IDP')
+		for file in CPFILES:
+			remotepath = '/var/tmp/IDP/%s'%file
+			localpath = '/var/tmp/IDPFiles/%s/%s'%(l,file)
+			SFTP.put(localpath, remotepath,callback=None, confirm=True)
+		SFTP.close()
+		transport.close()
+	return ()
 
 
 
@@ -106,8 +125,10 @@ global Build
 global version
 global unsername
 global List_IPs
+global Currentversion_List
 global inputfile
 global location
+global version_List
 
 location = []
 inputfile = []
@@ -116,7 +137,9 @@ device = []
 OS = []
 Build = []
 version = []
+Currentversion_List = []
 username = []
+version_List = []
 
 if len(sys.argv) > 1:
 	sysarguments()
@@ -131,8 +154,9 @@ for l in List_IPs:
 
 count = 0
 for l in List_IPs:
-    idpupdate(device[count], OS[count], location[0], Build[count],version[count],l)
+    idpupdate(device[count], OS[count], location[0], Build[count],Currentversion_List[count],l)
     count +=1
 
+movefiles_to_firewalls(username,password,List_IPs)
 
-print "Completed, please copy the files to the firewall idp location :  /var/db/idpd/sec-download/"
+print "Completed, Files are moved to the firewall nodes"
